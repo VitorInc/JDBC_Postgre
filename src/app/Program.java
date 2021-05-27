566cb25082579db854a02a43bcd111188d5a27c8
VitorInc <vitordc.contato@gmail.com>
2021-05-27 02:09:54 -0300

    entities Order and enum Ordertatus done

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Order;
+import entities.OrderStatus;
 import entities.Product;
 
 public class Program {
@@ -16,16 +18,33 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_product");
+		ResultSet rs = st.executeQuery("select * from tb_order");
 			
 		while (rs.next()) {
-			Product prod = new Product();
-			prod.setId(rs.getLong("Id"));
-			prod.setDescription(rs.getString("description"));
-			prod.setName(rs.getString("name"));
-			prod.setImageUrl(rs.getString("image_uri"));
-			prod.setPrice(rs.getDouble("price"));
-			System.out.println(prod);
+
+
+			Order order = instantiateOrder(rs);
+
+
+			System.out.println(order);
 		}
+	}
+	private static Product instantiateProduct(ResultSet rs) throws SQLException{
+		Product prod = new Product();
+		prod.setId(rs.getLong("Id"));
+		prod.setDescription(rs.getString("description"));
+		prod.setName(rs.getString("name"));
+		prod.setImageUrl(rs.getString("image_uri"));
+		prod.setPrice(rs.getDouble("price"));
+		return prod;
+	}
+	private static Order instantiateOrder(ResultSet rs) throws SQLException{
+		Order order = new Order();
+		order.setId(rs.getLong("Id"));
+		order.setLatitude(rs.getDouble("latitude"));
+		order.setLongitude(rs.getDouble("longitude"));
+		order.setMoment(rs.getTimestamp("moment").toInstant());
+		order.setStatus(OrderStatus.values()[rs.getInt("status")]);
+		return order;
 	}
 }
